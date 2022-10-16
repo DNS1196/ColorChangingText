62023807b4659fa996a8aac572d7e4354093a481
Denis Santana <denis@AHACK-PC.Transflo.com>
2022-10-16 01:18:14 -0400

    making changes

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+    return `rgba(${r},${g},${b},${Math.random()})`;
 }
 
-// const h1 = document.querySelector('h1')
-// setInterval(function () {
-//     h1.style.color = randomRGB();
-// }, 500);
 
 const letters = document.getElementsByClassName('letter');
 
